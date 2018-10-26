5016b4e1b38f066d0dc13eb99f046b0f97057789
gaoqi7 <gaoqi7@gmail.com>
2018-10-25 23:44:25 -0700

    [add items] function works,[add interval] function works,[file store]function works

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var fs = require("fs");
+var util = require("util");
+var data = require("./data/timesheet.json");
 var app = express();
 var isNew = true;
-var time = [
-  {
-    name: "javascript",
-    intervals: 0
-  }
-];
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -19,20 +16,32 @@ app.use(function(req, res, next) {
 });
 
 app.use(express.static("./public"));
+
 app.get("/time-api", function(req, res) {
-  res.json(time);
+  res.json(data);
+  console.log(data);
 });
+
 app.post("/time-api", function(req, res) {
-  time.forEach(function(elem) {
+  data.forEach(function(elem) {
     if (elem.name === req.body.name) {
       elem.intervals++;
       isNew = false;
-      res.json(time);
+      res.json(data);
+      // let b = JSON.stringify(data);
+      fs.writeFile("./data/timesheet.json", JSON.stringify(data), function(
+        err
+      ) {
+        console.log(err);
+      });
     }
   });
   if (isNew) {
-    time.push(req.body);
-    res.json(time);
+    data.push(req.body);
+    res.json(data);
+    fs.writeFile("./data/timesheet.json", JSON.stringify(data), function(err) {
+      console.log(err);
+    });
   }
   isNew = true;
 });
